78de6373363933a0d31724ef578e931247cb009e
neurowebstudioadmin <neurowebstudio@gmail.com>
2026-06-10 13:32:13 +0200

    Develplan Build: client/src/main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { StoreProvider } from './store'
-import './app.css'
-import './App.css'
-import './components.css'
+import './styles.css'
 
 const rootEl = document.getElementById('root')
 if (!rootEl) throw new Error('Root element #root not found')
