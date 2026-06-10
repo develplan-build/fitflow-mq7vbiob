f68da45bc720514d81175c10834ffb1a97c028af
neurowebstudioadmin <neurowebstudio@gmail.com>
2026-06-10 13:12:53 +0200

    Develplan Build: client/src/main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,11 @@
-*(già corretto, ma lo ripropongo per completezza)*
-```tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { StoreProvider } from './store'
-
-// Import globale dei CSS
-import './app.css'          // layout
-import './App.css'          // variabili e reset
-import './components.css'   // componenti UI
+import './app.css'
+import './App.css'
+import './components.css'
 
 const rootEl = document.getElementById('root')
 if (!rootEl) throw new Error('Root element #root not found')
@@ -23,4 +19,3 @@ ReactDOM.createRoot(rootEl).render(
     </BrowserRouter>
   </React.StrictMode>
 )
-```
