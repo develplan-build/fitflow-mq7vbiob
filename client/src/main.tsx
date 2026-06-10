0742c445b14974b11dd94ff6aef38087ff0cdcd2
neurowebstudioadmin <neurowebstudio@gmail.com>
2026-06-10 13:05:11 +0200

    Develplan Build: client/src/main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,14 @@
+```tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { StoreProvider } from './store'
-import './app.css'
+
+// Import di tutti i fogli di stile globali
+import './app.css'          // layout generale (grid, sidebar, header, ecc.)
+import './App.css'          // variabili CSS, reset, tipografia globale
+import './components.css'  // componenti UI riutilizzabili (modal, toast, badge, bottoni)
 
 const rootEl = document.getElementById('root')
 if (!rootEl) throw new Error('Root element #root not found')
@@ -17,3 +22,4 @@ ReactDOM.createRoot(rootEl).render(
     </BrowserRouter>
   </React.StrictMode>
 )
+```
